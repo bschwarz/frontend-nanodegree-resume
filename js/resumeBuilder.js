4a3236058abeda95a269a8d9e2525651946bb4f5
Brett Schwarz <bs595r@att.com>
2017-12-18 11:17:43 -0800

    Initial data objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,123 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//======================================================================
+// Resume Project for Udacity nanodegree Frontend Web Developer
+//======================================================================
+
+
+
+
+// -----------------------------------------
+//           Biography    
+// -----------------------------------------
+
+var bio = {
+  "name" : "Brett Schwarz",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "425-466-1731",
+    "email" : "brett.schwarz@gmail.com",
+    "github" : "bschwarz",
+    "twitter" : "",
+    "location" : "Seattle, USA" 
+  },
+  "welcomeMsg" : "Hi, welcome to my resume.",
+  "skills" : ["HTML5","CSS3","JavaScript","jQuery","Ajax","Tcl","C","SQL","MySQL","PostgreSQL","Bootstrap","Git","GitHub"],
+  "biopic" : "images/me.jpg",
+  display: function() {}
+};
+
+
+// -----------------------------------------
+//           Education   
+// -----------------------------------------
+
+var education = {
+  "schools" : [
+    {
+      "name" : "University of Toledeo",
+      "location" : "Toledo, Ohio",
+      "degree" : "Bachelor",
+      "majors" : ["Electrical Engineering", "Mathematics"],
+      "dates" : "1992",
+      "url" : "http://www.utoledo.edu"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Front End Web Development",
+      "school" : "Udacity",
+      "dates" : "2017",
+      "url" : "http://www.udacity.com"
+    }
+  ],
+  display: function() {}
+};
+
+
+// -----------------------------------------
+//           Work   
+// -----------------------------------------
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "AT&T",
+      "title" : "Principal System Engineer",
+      "location" : "Redmond, WA",
+      "dates" : "06/2015 - present",
+      "description" : ""
+    },
+    {
+      "employer" : "Sentaca",
+      "title" : "Solutions Architect",
+      "location" : "Redmond, WA",
+      "dates" : "06/2011 - 06/2015",
+      "description" : "" 
+    },
+    {
+      "employer" : "Contractor - AT&T",
+      "title" : "Senior Test Engineer",
+      "location" : "Redmond, WA",
+      "dates" : "10/2010 - 06/2011",
+      "description" : "" 
+    },
+    {
+      "employer" : "T-Mobile",
+      "title" : "Senior Engineer",
+      "location" : "Bellevue, WA",
+      "dates" : "05/2010 - 10/2010",
+      "description" : "" 
+    },
+    {
+      "employer" : "Alcatel-Lucent",
+      "title" : "Technical Sales Engineer",
+      "location" : "Bellevue, WA",
+      "dates" : "08/2008 - 09/2009",
+      "description" : ""
+    },
+    {
+      "employer" : "Alcatel-Lucent",
+      "title" : "Senior Network Systems Engineer",
+      "location" : "Bellevue, WA",
+      "dates" : "11/2005 - 08/2008",
+      "description" : ""
+    
+}  ],
+	display: function() {}
+};
+
+
+// -----------------------------------------
+//           Projects   
+// -----------------------------------------
+
+var projects = {
+  "projects" : [
+    {
+     "title" : "API Studio",
+     "dates" : "2017",
+     "description" : "Tool to document APIs",
+     "images" : [""]
+    }
+  ],
+  display: function() {}
+};
